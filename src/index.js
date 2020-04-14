1e24e222ec3dac846e84c5d2c35af79968996af5
Maria Trujillo <mptruj015@gmail.com>
2020-04-14 22:21:35 +0200

    created redux store and connected it to app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from 'react-redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { carReducer } from './reducers/carReducer'
+import { createStore } from 'redux';
+
+const store = createStore(carReducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
